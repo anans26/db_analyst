12fdc0f186eb66cd21b77b977c65a4e34d7c2386
ananya <mg.ananya@gmail.com>
2026-06-04 15:05:50 +0530

    added a scripts folder

---
+++ scripts/create_schema_v2.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from db import engine
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+files = [
+    BASE_DIR / "schema" / "layer1.sql",
+    BASE_DIR / "schema" / "layer2.sql",
+    BASE_DIR / "schema" / "layer3.sql",
+    BASE_DIR / "schema" / "layer4.sql"
+]
+
+with engine.raw_connection() as conn:
+    cursor = conn.cursor()
+
+    try:
+        for file_name in files:
+
+            with open(file_name, "r") as file:
+                sql = file.read()
+
+            cursor.execute(sql)
+
+        conn.commit()
+        print("Schema created successfully!")
+
+    except Exception as e:
+        conn.rollback()
+        print(f"Error creating schema: {e}")
+
+    finally:
+        cursor.close()
